4a72de864400e141c834c4769e1e6d2a6311112c
Kyle <732077007@qq.com>
2018-06-19 17:38:06 +0800

    first commit

---
+++ app/components/operation/airMock.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-row style="width: 100%">
+    </el-row>
+  </div>
+</template>
+<script>
+export default ({
+  name: 'air-mock',
+  props: {
+    test: Number
+  },
+  data () {
+    return {
+
+    }
+  }
+})
+</script>
